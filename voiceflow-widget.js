0ac13e0906c5fdd2c0894d85c736d99062d9d867
NovaAI-Games <pensi.edoardo@gmail.com>
2026-03-12 21:53:10 +0100

    Add files via upload

---
+++ voiceflow-widget.js
@@ -0,0 +1,16 @@
+(function(d, t) {
+    var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+    v.onload = function() {
+      window.voiceflow.chat.load({
+        verify: { projectID: '67d1fab25c5916e15e7d1e4c' },
+        url: 'https://general-runtime.voiceflow.com',
+        versionID: 'production',
+        voice: {
+          url: "https://runtime-api.voiceflow.com"
+        }
+      });
+    }
+    v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs";
+    v.type = "text/javascript";
+    s.parentNode.insertBefore(v, s);
+})(document, 'script');
